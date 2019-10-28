0b953b5793320c2f64592ae1dcdb855da1e756cc
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2019-10-28 01:26:46 -0600

    add ios changes and use cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //Imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
